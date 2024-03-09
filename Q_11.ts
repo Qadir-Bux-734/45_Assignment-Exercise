8db130c0fb6590adc7d5f9f85e884f717bf0ac16
Qadir-Bux-734 <160654196+Qadir-Bux-734@users.noreply.github.com>
2024-03-09 23:48:17 +0500

    Add files via upload

---
+++ Q_11.ts
@@ -0,0 +1,11 @@
+// 11.Store the names of a few of your friends in a array called names. Print each person’s name by accessing each element in the list, one at a time.
+
+
+
+let friendsName1:string[]=["Qadir","Uzair","Ali","Raza" ,"zulfiqar"]
+//      index arrays       [0]      [1]    [2]   [3]       [4]
+console.log(friendsName1[0]);
+console.log(friendsName1[1]);
+console.log(friendsName1[2]);
+console.log(friendsName1[3]);
+console.log(friendsName1[4]);
